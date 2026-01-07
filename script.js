658575e20f46c20ff053e942dd997f7a47c0a9bd
Nicholai Fischer Larsen <nicholaifischer@MacBook-Air-tilhrende-Nicholai.local>
2026-01-07 19:49:21 +0100

    Implement Light/Dark mode switch

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Portfolio Loaded');
-    
+
     // Smooth scroll for anchor links
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
@@ -10,4 +10,29 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         });
     });
+
+    // Theme Toggle
+    const themeToggle = document.querySelector('.theme-toggle');
+    const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
+
+    // Check for saved user preference, if any, on load of the website
+    const currentTheme = localStorage.getItem('theme');
+    if (currentTheme == 'dark') {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    } else if (currentTheme == 'light') {
+        document.documentElement.setAttribute('data-theme', 'light');
+    } else if (prefersDarkScheme.matches) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }
+
+    if (themeToggle) {
+        themeToggle.addEventListener('click', function () {
+            let theme = 'light';
+            if (document.documentElement.getAttribute('data-theme') !== 'dark') {
+                theme = 'dark';
+            }
+            document.documentElement.setAttribute('data-theme', theme);
+            localStorage.setItem('theme', theme);
+        });
+    }
 });
